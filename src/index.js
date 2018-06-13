1503d46c25a725ace6b2e6bffac633caa8080fdb
Viktor Zhurbin <vzhurbin@gmail.com>
2018-06-13 09:25:59 +0300

    create-react app + file structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import './index.css'
+import App from './components/App'
+import reducer from './reducers'
+import middleware from './middleware'
+
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
